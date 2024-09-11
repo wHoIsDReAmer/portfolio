7979348137c05a7aaec6b0a61c49ea17b59d41dc
wHoIsDReAmer <lcw060403@gmail.com>
2024-09-11 19:54:02 +0900

    feat: add hybrid mode and add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  base: '/portfolio/',
+  output: 'hybrid',
+  site: 'https://whoisdreamer.github.io/portfolio/',
+  adapter: vercel()
 });
